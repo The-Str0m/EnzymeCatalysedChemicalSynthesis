aeafeb1bf26a2ca5a3609f988cd3699171d46c6d
The-Str0m <pranavsagiraju@gmail.com>
2026-07-18 09:17:59 +0530

    Add files via upload
    pink detection algorithm to check start/end point of reaction

---
+++ pink_detection.py
@@ -0,0 +1,60 @@
+from fcntl import LOCK_WRITE
+import cv2
+import numpy as np
+
+
+cam_url = "http://172.20.10.12:81/stream"
+
+#minimum threshold for pink
+threshold = 40.0
+
+#pink thresholds
+lower_pink = np.array([140, 50, 50])
+upper_pink = np.array([170, 255, 255])
+
+#capture stream
+cap = cv2.VideoCapture(cam_url)
+
+if not cap.isOpened():
+    print("couldn't connect to video stream")
+    exit()
+
+print("stream processing")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("couldn't fetch frames from camera")
+        break
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    #mask
+    pink_mask = cv2.inRange(hsv_frame, lower_pink, upper_pink)
+
+    #calculate percentage
+    total_pixels = frame.shape[0] * frame.shape[1]
+    pink_pixels = cv2.countNonZero(pink_mask)
+    pink_percentage = (pink_pixels / total_pixels) * 100
+
+
+    if pink_percentage >= threshold:
+        alert_text = f"pink detected ({pink_percentage:.1f}%)"
+        text_color = (0, 0, 255)
+    else:
+        alert_text = f"searched for pink {pink_percentage:.1f}%"
+        text_color = (0, 255, 0)
+
+    cv2.putText(frame, alert_text, (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 
+                1, text_color, 2, cv2.LINE_AA)
+
+
+    cv2.imshow("live stream processing", frame)
+    cv2.imshow("pink mask", pink_mask)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
